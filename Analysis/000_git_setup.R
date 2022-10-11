eb151ef19267e0e000274df03a030a957c7fdaba
vermanica <veronicaann621@gmail.com>
2022-10-11 10:13:10 -0400

    adding script to setup github on office computer

---
+++ Analysis/000_git_setup.R
@@ -0,0 +1,21 @@
+# Setting up github
+# 10/11/2022
+
+library(usethis)
+
+
+use_git_config(user.name = "vermanica",
+               user.email = "veronicaann621@gmail.com")
+
+git_default_branch_configure()
+
+use_git_remote(
+  "origin",
+  "https://github.com/vermanica/eHSF.git",
+  overwrite = T
+)
+
+create_github_token()
+# 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
+
+gitcreds::gitcreds_set()
