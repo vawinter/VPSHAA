bc6bacdc2fef63e0cc9ed95e9a79cb17f0c52bfe
vermanica <veronicaann621@gmail.com>
2022-10-20 14:34:42 -0400

    Formatting tables for overleaf integration for manuscript

---
+++ Analysis/15_tables.R
@@ -0,0 +1,147 @@
+
+# Formatting tables for overleaf
+
+# Libraries
+library(dplyr)
+library(lubridate)
+library(DBI)
+library(knitr)
+library(kableExtra)
+
+# Table 1: references for covaraites ----
+table1 <- data.frame("Covariates" = c("Elevation", "Roughness","Aspect (Easting)", "Aspect (Northing)",
+                                      "Herbaceous cover", "Shrub cover", "Tree cover"),
+                     "Temporally Static/Dynamic" = c(rep("static", 4), rep("dynamic", 3)),
+                     "Expected effect" = c(rep("avoidance", 3), rep("selection", 3), rep("avoidance", 1)),
+                     "References" = NA,
+                       
+                       # c("(Beale and Smith 1970, O’Gara et al. 2004, Zeller et al. 2021)",
+                       #                "(Beale and Smith 1970, O’Gara et al. 2004)",
+                       #                rep("(Guisan et al. 1999, Maggini et al. 2002, Hirzel and Le Lay 2008)",2),
+                       #                "(Aikens et al. 2017, 2020b)",
+                       #                "(Beale and Smith 1970a, O’Gara et al. 2004, Berger 2004)",
+                       #                "(O’Gara et al. 2004, Larsen et al. 2011)"),
+                     check.names = FALSE) %>% 
+  knitr::kable(booktabs = T,
+               escape = F,
+               caption = "Fine-scale habitat covaraites thought to be important 
+               to pronghorn ecology and hence used as eHSF predictors (steps 4).
+               All Covariates were measured at 30m resolution.",
+               format = "latex",
+               align = c("lrccccccccc")) %>% 
+  column_spec(2,bold=T,latex_column_spec = ">{\\\\color{black}}c") %>% 
+  collapse_rows(columns = 2, latex_hline = "major",valign = "middle") %>%
+  kable_styling(latex_options = c("hold_position", "repeat_header", "striped"), full_width = FALSE)
+
+writeClipboard(table1)
+
+
+# Table 2: references for other supporting variables ----
+table2 <- data.frame("Variables" = c("Mean Elevation", "Mean Roughness","Mean Easting", "Mean Northing",
+                                      "Mean Herbaceous Cover", "Mean Shrub Cover", "Mean Tree Cover",
+                                      "Mean Road Density", "Mean Snow Depth", "Mean Herbaceous Biomass",
+                                      "Mean Palmer Drought Severity Index"),
+                     "Temporally Static/Dynamic" = c(rep("static", 4), rep("dynamic", 3),"static",
+                                                     rep("dynamic", 3)),
+                     "References" = NA,
+                     
+                     # c("(Beale and Smith 1970, O’Gara et al. 2004, Zeller et al. 2021)",
+                     #                "(Beale and Smith 1970, O’Gara et al. 2004)",
+                     #                rep("(Guisan et al. 1999, Maggini et al. 2002, Hirzel and Le Lay 2008)",2),
+                     #                "(Aikens et al. 2017, 2020b)",
+                     #                "(Beale and Smith 1970a, O’Gara et al. 2004, Berger 2004)",
+                     #                "(O’Gara et al. 2004, Larsen et al. 2011)"),
+                     check.names = FALSE) %>% 
+  knitr::kable(booktabs = T,
+               escape = F,
+               caption = "Coarse-scale habitat covariates expected to influence 
+               the selection for the seven fine-scale covariates listed in 'Environmental
+               Covaraites'",
+               format = "latex",
+               align = c("lrccccccccc")) %>% 
+  column_spec(2,bold=T,latex_column_spec = ">{\\\\color{black}}c") %>% 
+  collapse_rows(columns = 2, latex_hline = "major",valign = "middle") %>%
+  kable_styling(latex_options = c("hold_position", "repeat_header", "striped"), full_width = FALSE)
+
+writeClipboard(table2)
+
+# Table 3: Migration summaries ----
+## Load in data 
+
+mig <- read.csv("Data/Processed/mig-comb_20220628.csv", header = T)
+
+table(mig$year, mig$tendency, mig$mig_season)
+
+mig_sum <- mig %>% 
+  filter(mig_season == "S",
+         year)
+
+mig_tab <- data.frame("Year" = c("2018", "2018", "2019", "2019", "2020","2020",
+                                 "2021", "2021"),
+                      "Migration Period" = c("Spring", "Fall", "Spring", "Fall",
+                                             "Spring", "Fall", "Spring", "Fall"),
+                      "Range-shifter" = c("35", "29", "25", "52", 
+                                          "43", "28", "75", "80"),
+                      "Resident" = c("20", "36", "23", "19", "18", "19", "8", "6"),
+                      "Nomadic" = c("45", "35", "52", "29", "39", "53", "17", "14"),
+                      check.names = FALSE) %>% 
+  knitr::kable(booktabs = T,
+               escape = F,
+               caption = "Observed parentage of bi-annual pronghorn movement 
+               in Utah between 2018-2021, grouped by migratory status 
+               (range-shifting, resident, and nomadic). Percentages were 
+               calculated based out of 100 on total collared population for 
+               that season/year.",
+               format = "latex",
+               align = c("lrccccccccc")) %>% 
+ # column_spec(2,bold=T,latex_column_spec = ">{\\\\color{black}}c") %>% 
+ # collapse_rows(columns = 2, latex_hline = "major",valign = "middle") %>%
+  kable_styling(latex_options = c("hold_position", "repeat_header", "striped"), full_width = FALSE)
+
+writeClipboard(mig_tab)
+
+# table 4 goodness of fit ----
+table4 <- data.frame("Covariates" = c("Elevation", "Roughness","Aspect (Easting)", "Aspect (Northing)",
+                                      "Herbaceous cover", "Shrub cover", "Tree cover"),
+                     "R² (marginal)" = rep(NA),
+                     "R² (conditional)" = NA,
+                     check.names = FALSE) %>% 
+  knitr::kable(booktabs = T,
+               escape = F,
+               caption = "Goodness of fit: Conditional and Marginal R² 
+               values for each covariate model output.",
+               format = "latex",
+               align = c("lrccccccccc")) %>% 
+  #column_spec(2,bold=T,latex_column_spec = ">{\\\\color{black}}c") %>% 
+ # collapse_rows(columns = 2, latex_hline = "major",valign = "middle") %>%
+  kable_styling(latex_options = c("hold_position", "repeat_header", "striped"), full_width = FALSE)
+
+writeClipboard(table4)
+
+
+# table 5
+
+table5 <- data.frame("Obsereved vs Predicted 
+                     Selection Coefficients" = 
+                       c("Elevation", "Roughness","Aspect (Easting)", "Aspect (Northing)",
+                                      "Herbaceous cover", "Shrub cover", "Tree cover"),
+                     "MAE FUll" = rep(NA),
+                     "MAE Null" = NA,
+                     "Pseudo R²" = NA,
+                     check.names = FALSE) %>% 
+  knitr::kable(booktabs = T,
+               escape = F,
+               caption = "Out of sample validation: Mean Absolute Error 
+               (MAE) when applied to out-of-sample data for predictions of 
+               full vs null model, and the difference between the two. 
+               Positive pseudo-R² values indicate the predictive performance 
+               of the full covariate model was better than the null covariate 
+               model. Negative values indicate that the null covariate model 
+               had better predictive performance than the full covariate model.",
+               format = "latex",
+               align = c("lrccccccccc")) %>% 
+  #column_spec(2,bold=T,latex_column_spec = ">{\\\\color{black}}c") %>% 
+  # collapse_rows(columns = 2, latex_hline = "major",valign = "middle") %>%
+  kable_styling(latex_options = c("hold_position", "repeat_header", "striped"), full_width = FALSE)
+
+writeClipboard(table5)
