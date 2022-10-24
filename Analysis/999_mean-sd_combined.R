48b71819a53a4eb5c5011e48b57e143ac84634f9
vermanica <veronicaann621@gmail.com>
2022-10-24 13:43:19 -0400

    mean/sd calculaitons and formatting

--- Analysis/999_mean-sd_combined.R
+++ Analysis/999_mean-sd_combined.R
@@ -8,8 +8,14 @@ x <- read.csv("Data/Processed/Mean_sd/20221018_mean-sd-preRSF.csv", header = T)
 y <- read.csv("Data/Processed/Mean_sd/20220718_mean-sd_snow-road.csv", header = T)
 
 # join together
-all <- merge(x, y)
+all <- cbind(x, y)
+row.names(all)[1] <- "mean"
+row.names(all)[2] <- "sd"
+all
+
+all$SND <- NULL
+all
 
 # save 
-write.csv(all, "Data/Processed/Mean_sd/20221019_mean-sd_all.csv", row.names = F)
-write.csv(all, "../Winter_etal_map/20221019_mean-sd_all.csv", row.names = F)
+write.csv(all, "Data/Processed/Mean_sd/20221019_mean-sd_all.csv", row.names = T)
+write.csv(all, "../Winter_etal_map/20221019_mean-sd_all.csv", row.names = T)
